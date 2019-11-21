2d0f7d16909fdcdba99c9d2b7651c4abd758e534
daily111 <16500430818@qq.com>
2019-11-21 14:25:26 +0800

    Initial commit

---
+++ blogs/blogs_web/src/main/java/com/BlogsWeb_80.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsWeb_80 {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsWeb_80.class, args);
+    }
+
+}
